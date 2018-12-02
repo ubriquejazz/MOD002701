392e0f4542c49cf169931eef97ed1963789c4cbd
ubriquejazz <ubriquejazz@gmail.com>
2018-12-02 10:38:06 +0000

    public nav

---
+++ private/shared/public_navigation.php
@@ -0,0 +1,19 @@
+<?php
+  // Default values to prevent errors
+  $subject_id = $subject_id ?? '';
+?>
+
+<navigation>
+  <?php $nav_subjects = find_all_subjects(); ?>
+  <ul class="subjects">
+    <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { ?>
+        <li class="<?php if($nav_subject['id'] == $subject_id) { echo 'selected'; } ?>">
+        <a href="<?php echo url_for('index.php?id=' . h(u($nav_subjects['id'])) ); ?>">
+          <?php echo h($nav_subjects['menu_name']); ?>
+        </a>
+
+      </li>
+    <?php } // while $nav_subjects ?>
+  </ul>
+  <?php mysqli_free_result($nav_subjects); ?>
+</navigation>
--- public/index.php
+++ public/index.php
@@ -1,14 +1,54 @@
-<!doctype html>
+<?php require_once('../private/initialize.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>Globe Bank</title>
-    <meta charset="utf-8">
-  </head>
+<?php
+if(isset($_GET['id'])) {
+  $subject_id = $_GET['id'];
+  $subject = find_subject_by_id($subject_id);
+  if(!$subject) {
+    redirect_to(url_for('/index.php'));
+  }
+} else {
+  // nothing selected; show the homepage
+}
 
-  <body>
+?>
 
-    <h1>Globe Bank: Coming Soon</h1>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
 
-  </body>
-</html>
+<div id="main">
+
+  <?php include(SHARED_PATH . '/public_navigation.php'); ?>
+
+  <div id="subject">
+
+    <?php
+      if(isset($subject)) {        
+        // echo $page['content'];
+        $nav_subjects = find_pages_by_subject_id($subject['id']);
+    ?>
+        <ul class="subjects">
+          <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { ?>
+            <li>
+              <a href="<?php echo url_for('index.php?id=' . h(u($nav_subject['id'])) ); ?>">
+                <?php echo h($nav_subject['menu_name']); ?>
+              </a>
+            </li>
+          <?php } // while $nav_subjects ?>
+        </ul>
+
+    <?php
+      } else {
+        // Show the homepage
+        // The homepage content could:
+        // * be static content (here or in a shared file)
+        // * show the first page from the nav
+        // * be in the database but add code to hide in the nav
+        include(SHARED_PATH . '/static_homepage.php');
+      }
+    ?>
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
