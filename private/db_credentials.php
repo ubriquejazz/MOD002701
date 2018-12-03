8f85a7e351b5989b20fd22eac6a24ccad01590d7
ubriquejazz <ubriquejazz@gmail.com>
2018-12-03 14:25:51 +0000

    admin area

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,8 @@
 <?php
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "secretpassword");
-define("DB_NAME", "globe_bank");
+define("DB_USER", "root");
+define("DB_PASS", "secret");
+define("DB_NAME", "biblio");
 
 ?>
